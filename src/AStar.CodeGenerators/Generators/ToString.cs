6cdf5d97b98a7455a611fcef45b6662a5dd078a8
ADO Release Testing <MACE-Ways-of-Working@capgeminicsduk.com>
2024-04-01 20:14:16 +0100

    Add the code to generate the GenerateToStringAttribute
    Have included some comments but will check whether I need, as I suspect, the "generate XML Docs" etc. too

--- src/AStar.CodeGenerators/Generators/ToString.cs
+++ src/AStar.CodeGenerators/Generators/ToString.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 
-namespace astar_codegenerators.Generators
+namespace AStar.CodeGenerators.Generators;
+
+[Generator]
+internal class ToString : IIncrementalGenerator
 {
-    [Generator]
-    internal class ToString : IIncrementalGenerator
+    public void Initialize(IncrementalGeneratorInitializationContext context) =>
+        context.RegisterPostInitializationOutput(
+            static (context) => PostInitializationOutput(context));
+
+    private static void PostInitializationOutput(
+        IncrementalGeneratorPostInitializationContext context)
     {
-        public void Initialize(IncrementalGeneratorInitializationContext context) => throw new NotImplementedException();
+        context.AddSource("AStar.CodeGenerators.GenerateToStringAttribute.g.cs",
+@"namespace AStar.CodeGenerators;
+
+/// <summary>
+/// The GenerateToString attribute is inteaded to be used on classes to simplify creating, as you would expect, the overridden ToString() method.
+/// <para>
+/// There will be additional attributes created shortly that will allow for Redacting, Masking or Ignoring specific properties. Any public property without one of these attributes will be included in the ToString output.
+/// </para>
+/// </summary>
+[System.AttributeUsage(System.AttributeTargets.Class, Inherited = false)]
+internal sealed class GenerateToStringAttribute : System.Attribute { }");
     }
 }
