a30f414deca80e7f520dd149ea0c72a7f2bf5129
ADO Release Testing <MACE-Ways-of-Working@capgeminicsduk.com>
2024-04-01 19:29:11 +0100

    Add initial version of the project structure

---
+++ src/AStar.CodeGenerators/astar-codegenerators/Generators/ToString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.CodeAnalysis;
+
+namespace astar_codegenerators.Generators
+{
+    [Generator]
+    internal class ToString : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context) => throw new NotImplementedException();
+    }
+}
---
+++ tests/astar-codegenerators.Unit.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace astar_codegenerators.Unit.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
